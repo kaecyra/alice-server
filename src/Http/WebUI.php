4848d495ecfe7b814f49dd9e799cdb87fae57716
Timothy Gunter <tim@vanillaforums.com>
2016-06-08 01:18:12 -0400

    Add connection sleep

--- src/Http/WebUI.php
+++ src/Http/WebUI.php
@@ -159,6 +159,7 @@ class WebUI extends App {
             $context = new ZMQContext();
             $publisher = $context->getSocket(ZMQ::SOCKET_PUB);
             $publisher->connect("tcp://{$zmqConfig['host']}:{$zmqConfig['port']}");
+            usleep(200); // Allow time to connect
 
             $update = sprintf("%s: %s", 'sensor-messages', serialize($message));
             $publisher->send($update);
